63ca2079a7673d55c56626b65d14b941e410c588
Melvunx <augustine.melv@gmmail.com>
2024-10-18 15:51:21 +0200

    setup form

--- client/src/components/layout/Form.tsx
+++ client/src/components/layout/Form.tsx
@@ -8,7 +8,7 @@ const Form = () => {
   const [applyDate, setApplyDate] = useState("");
   const [status, setStatus] = useState("Pas encore envoyée");
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
@@ -19,6 +19,12 @@ const Form = () => {
         applyDate,
         status,
       });
+      console.log("Offre ajoutée : ", res.data);
+      setTitle("");
+      setCompany("");
+      setUrl("");
+      setApplyDate("");
+      setStatus("");
     } catch (err) {
       console.error("Erreur lors de l'ajout d'offre : " + err);
     }
@@ -26,7 +32,61 @@ const Form = () => {
 
   return (
     <div>
-      <form></form>
+      <form onSubmit={handleSubmit}>
+        <h1>Ajouter une demande d'alternance</h1>
+        <div className="">
+          <label htmlFor="title" className="">
+            Titre de l'offre
+          </label>
+          <input
+            type="text"
+            name="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </div>
+        <div className="">
+          <label htmlFor="company" className="">
+            Nom de l'entreprise
+          </label>
+          <input
+            type="text"
+            name="company"
+            value={company}
+            onChange={(e) => setCompany(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="url">Lien de l'offre</label>
+          <input
+            type="text"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="apply">Date de candidature</label>
+          <input
+            type="date"
+            value={applyDate}
+            onChange={(e) => setApplyDate(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="status">Status</label>
+          <select name="status" onChange={(e) => setStatus(e.target.value)}>
+            <option value="Pas envoyé">Pas envoyé</option>
+            <option value="Envoyé">Envoyé</option>
+            <option value="Refusé">Refusé</option>
+            <option value="Accepté">Accepté</option>
+          </select>
+        </div>
+        <button type="submit">Ajouter</button>
+      </form>
     </div>
   );
 };
