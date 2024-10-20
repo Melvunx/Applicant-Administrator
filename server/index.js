54f493a98ccd51c2061b9ce320b59fe26cae7095
Melvunx <augustine.melv@gmmail.com>
2024-10-20 15:02:31 +0200

    week mailing

--- server/index.js
+++ server/index.js
@@ -48,7 +48,6 @@ Toi même.
 const sendMail = async (transporter, mailOptions) => {
   try {
     await transporter.sendMail(mailOptions);
-    console.log("L'email à bien été envoyée !");
   } catch (error) {
     console.error("Une erreur est survenue lors de l'envoie du mail ! ", error);
   }
@@ -62,60 +61,66 @@ transporter.verify((error, succes) => {
 });
 
 //Envoie de tâche planifiée
-cron.schedule("* * * * *", async () => {
-  console.log("Vérification des offres envoyées il y a 1 minute...");
+cron.schedule("0 10 * * *", async () => {
+  console.log("Vérification des offres envoyées il y a une semaine...");
 
-  const oneMinuteAgo = new Date();
-  oneMinuteAgo.setMinutes(oneMinuteAgo.getMinutes() - 1);
+  const oneWeekAgo = new Date();
+  oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
 
   try {
     const offers = await offerModel.find({
-      applyDate: { $lte: oneMinuteAgo },
+      applyDate: { $lte: oneWeekAgo },
       archived: false,
       status: "Envoyé",
     });
 
     //Envoie d'email
-    offers.forEach(async (offer) => {
-      console.log("Offres  envoyées il y a 1 minute : ", offer);
-      const relanceMail = {
-        from: {
-          name: "Rappeleur de relance",
-          address: process.env.USER_ADDRESS,
-        },
-        to: [process.env.USER_ADDRESS],
-        subject: `Relance de Candidature - 1 minute depuis ma candidature chez ${offer.company}`,
-        html: `
-          <div style="font-family: Arial, sans-serif; color: #333; padding: 20px; background-color: #f9f9f9; border: 1px solid #ddd; border-radius: 10px; max-width: 600px; margin: auto;">
-            <h1 style="font-size: 24px; color: #4CAF50;">Bonjour mon Melv,</h1>
-            <p style="font-size: 16px; line-height: 1.6;">
-              Cela fait <strong>1 minute</strong> que tu as postulé chez 
-              <strong>${offer.company}</strong> avec 
-              ${
-                offer.type === "Candidature spontanée"
-                  ? "<em>une candidature spontanée</em>"
-                  : `l'offre <strong>${offer.title}</strong>`
-              }.
-            </p>
-            <p style="font-size: 16px; line-height: 1.6;">
-              Il est temps de relancer ta candidature en renvoyant un e-mail. Ne perds pas de temps !
-            </p>
-            <div style="margin-top: 20px; text-align: center;">
-              <a href="${
-                offer.url
-              }" style="display: inline-block; padding: 12px 24px; background-color: #4CAF50; color: white; text-decoration: none; border-radius: 5px; font-size: 16px;">Voir l'offre</a>
+    if (offers.length === 0) {
+      console.log("Aucune offre trouvée pour l'envoi de mail");
+    } else {
+      offers.forEach(async (offer) => {
+        console.log("Offres  envoyées il y a 1 minute : ", offer);
+        const relanceMail = {
+          from: {
+            name: "Rappeleur de relance",
+            address: process.env.USER_ADDRESS,
+          },
+          to: [process.env.USER_ADDRESS],
+          subject: `Relance de Candidature - 1 minute depuis ma candidature chez ${offer.company}`,
+          html: `
+            <div style="font-family: Arial, sans-serif; color: #333; padding: 20px; background-color: #f9f9f9; border: 1px solid #ddd; border-radius: 10px; max-width: 600px; margin: auto;">
+              <h1 style="font-size: 24px; color: #4CAF50;">Bonjour mon Melv,</h1>
+              <p style="font-size: 16px; line-height: 1.6;">
+                Cela fait <strong>1 minute</strong> que tu as postulé chez 
+                <strong>${offer.company}</strong> avec 
+                ${
+                  offer.type === "Candidature spontanée"
+                    ? "<em>une candidature spontanée</em>"
+                    : `l'offre <strong>${offer.title}</strong>`
+                }.
+              </p>
+              <p style="font-size: 16px; line-height: 1.6;">
+                Il est temps de relancer ta candidature en renvoyant un e-mail. Ne perds pas de temps !
+              </p>
+              <div style="margin-top: 20px; text-align: center;">
+                <a href="${
+                  offer.url
+                }" style="display: inline-block; padding: 12px 24px; background-color: #4CAF50; color: white; text-decoration: none; border-radius: 5px; font-size: 16px;">Voir l'offre</a>
+              </div>
+              <br/>
+              <p style="font-size: 16px; line-height: 1.6;">
+                Cordialement,<br/>
+                Toi même.<br/>
+              </p>
             </div>
-            <br/>
-            <p style="font-size: 16px; line-height: 1.6;">
-              Cordialement,<br/>
-              Toi même.<br/>
-            </p>
-          </div>
-        `,
-      };
-
-      await sendMail(transporter, relanceMail);
-    });
+          `,
+        };
+        await sendMail(transporter, relanceMail);
+        console.log(
+          `L'email de relance pour l'entreprise ${offer.company} a bien été envoyé !`
+        );
+      });
+    }
   } catch (error) {
     console.error("Erreur lors de la vérification des offres : ", error);
   }
