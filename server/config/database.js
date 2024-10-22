3bc25b427eba174c59e050425bd4798e797993be
Melvunx <augustine.melv@gmmail.com>
2024-10-22 18:32:59 +0200

    data base access

--- server/config/database.js
+++ server/config/database.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 
 mongoose
-  .connect(process.env.MONGODB_URL, { serverSelectionTimeoutMS: 30000 })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDb !"))
   .catch((err) => console.log(err));
