11021b0587d47cd72124136aadf67d33ab154db3
Melvunx <augustine.melv@gmmail.com>
2024-11-02 10:44:09 +0100

    bug fix #2

--- server/api/cron.js
+++ server/api/cron.js
@@ -1,11 +1,15 @@
-const sendScheduledEmails = require("../index");
+const { sendScheduledEmails } = require("../index");
 
 export default async function handler(req, res) {
   if (req.headers["authorization"] !== `Bearer ${process.env.CRON_SECRET}`) {
     return res.status(401).end("Unauthorized");
   } else if (req.method === "GET") {
-    await sendScheduledEmails();
-    res.status(200).json({ message: "Scheduled task executed successfully" });
+    try {
+      await sendScheduledEmails();
+      res.status(200).json({ message: "Scheduled task executed successfully" });
+    } catch (error) {
+      res.status(500).json({ message: "Error executing scheduled task", error: error.message });
+    }
   } else {
     res.status(405).json({ message: "Method not allowed" });
   }
