4f240fc9b0198ed7af1a275fd607a83089b5644a
Melvunx <augustine.melv@gmmail.com>
2024-11-02 13:03:17 +0100

    testing code

--- server/api/cron.js
+++ server/api/cron.js
@@ -1,5 +1,5 @@
-const nodemailer = require("nodemailer");
-const offerModel = require("../models/offer.models");
+import nodemailer from "nodemailer";
+import offerModel from "../models/offer.models";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -22,8 +22,6 @@ const sendMail = async (transporter, mailOptions) => {
   }
 };
 
-//Envoi de tâche planifiée
-
 export default async function handler(req, res) {
   if (req.headers["authorization"] !== `Bearer ${process.env.CRON_SECRET}`) {
     return res.status(401).end("Unauthorized");
@@ -31,123 +29,84 @@ export default async function handler(req, res) {
     try {
       // Vérifier la connexion SMTP
       await transporter.verify();
-
       console.log("Le serveur SMTP est prêt à prendre nos messages !");
 
-      // Appeler la fonction d'envoi d'emails
-      const sendScheduledEmails = async () => {
-        console.log("Vérification des offres envoyées il y a une semaine...");
+      // Récupération des offres
+      const oneWeekAgo = new Date();
+      oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+      console.log("Date de comparaison : ", oneWeekAgo);
 
-        const oneWeekAgo = new Date();
-        oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+      const offers = await offerModel
+        .find({
+          applyDate: { $lte: oneWeekAgo },
+          archived: false,
+          status: "Envoyé",
+        })
+        .exec();
 
-        console.log("Date de comparaison : ", oneWeekAgo);
+      console.log("Offres récupérées : ", offers);
 
-        const retryFetch = async (retries) => {
-          for (let i = 0; i < retries; i++) {
-            try {
-              return await offerModel
-                .find({
-                  applyDate: { $lte: oneWeekAgo },
-                  archived: false,
-                  status: "Envoyé",
-                })
-                .exec();
-            } catch (error) {
-              console.error(
-                "Erreur de requête, tentative de nouvelle... ",
-                error
-              );
-              await new Promise((res) => setTimeout(res, 2000)); // Attendre 2 secondes avant de réessayer
-            }
-          }
-          throw new Error(
-            "Échec de la récupération des offres après plusieurs tentatives."
-          );
+      if (offers.length === 0) {
+        console.log("Aucune offre trouvée pour l'envoi de mail");
+        return res.status(200).json({ message: "Aucune offre à relancer." });
+      }
+
+      for (const offer of offers) {
+        console.log("Traitement de l'offre : ", offer.company);
+
+        const relanceMail = {
+          from: {
+            name: "Rappeleur de relance",
+            address: process.env.USER_ADDRESS,
+          },
+          to: process.env.USER_ADDRESS,
+          subject: `Relance de Candidature - Une semaine depuis ma candidature chez ${offer.company}`,
+          html: `
+            <div style="font-family: Arial, sans-serif; color: #333; padding: 20px; background-color: #f9f9f9; border: 1px solid #ddd; border-radius: 10px; max-width: 600px; margin: auto;">
+              <h1 style="font-size: 24px; color: #4CAF50;">Bonjour mon Melv,</h1>
+              <p style="font-size: 16px; line-height: 1.6;">
+                Cela fait <strong>une semaine</strong> que tu as postulé chez 
+                <strong>${offer.company}</strong> avec 
+                ${
+                  offer.type === "Candidature spontanée"
+                    ? "<em>une candidature spontanée</em>"
+                    : `l'offre <strong>${offer.title}</strong>`
+                }.
+              </p>
+              <p style="font-size: 16px; line-height: 1.6;">
+                Il est temps de relancer ta candidature en renvoyant un e-mail. Ne perds pas de temps !
+              </p>
+              <div style="margin-top: 20px; text-align: center;">
+                <a href="${
+                  offer.url
+                }" style="display: inline-block; padding: 12px 24px; background-color: #4CAF50; color: white; text-decoration: none; border-radius: 5px; font-size: 16px;">Voir l'offre</a>
+              </div>
+              <br/>
+              <p style="font-size: 16px; line-height: 1.6;">
+                Cordialement,<br/>
+                Toi même.<br/>
+              </p>
+            </div>
+          `,
         };
 
-        try {
-          const offers = await retryFetch(3);
-          console.log("Offres récupérées : ", offers);
+        await sendMail(transporter, relanceMail);
+        console.log(
+          `L'email de relance pour l'entreprise ${offer.company} a bien été envoyé !`
+        );
+      }
 
-          if (offers.length === 0) {
-            console.log("Aucune offre trouvée pour l'envoi de mail");
-          } else {
-            for (const offer of offers) {
-              console.log("Traitement de l'offre : ", offer.company);
-
-              const relanceMail = {
-                from: {
-                  name: "Rappeleur de relance",
-                  address: process.env.USER_ADDRESS,
-                },
-                to: process.env.USER_ADDRESS,
-                subject: `Relance de Candidature - Une semaine depuis ma candidature chez ${offer.company}`,
-                html: `
-                  <div style="font-family: Arial, sans-serif; color: #333; padding: 20px; background-color: #f9f9f9; border: 1px solid #ddd; border-radius: 10px; max-width: 600px; margin: auto;">
-                    <h1 style="font-size: 24px; color: #4CAF50;">Bonjour mon Melv,</h1>
-                    <p style="font-size: 16px; line-height: 1.6;">
-                      Cela fait <strong>une semaine</strong> que tu as postulé chez 
-                      <strong>${offer.company}</strong> avec 
-                      ${
-                        offer.type === "Candidature spontanée"
-                          ? "<em>une candidature spontanée</em>"
-                          : `l'offre <strong>${offer.title}</strong>`
-                      }.
-                    </p>
-                    <p style="font-size: 16px; line-height: 1.6;">
-                      Il est temps de relancer ta candidature en renvoyant un e-mail. Ne perds pas de temps !
-                    </p>
-                    <div style="margin-top: 20px; text-align: center;">
-                      <a href="${
-                        offer.url
-                      }" style="display: inline-block; padding: 12px 24px; background-color: #4CAF50; color: white; text-decoration: none; border-radius: 5px; font-size: 16px;">Voir l'offre</a>
-                    </div>
-                    <br/>
-                    <p style="font-size: 16px; line-height: 1.6;">
-                      Cordialement,<br/>
-                      Toi même.<br/>
-                    </p>
-                  </div>
-                `,
-              };
-
-              // Enlever `await` pour tester s'il s'agit d'un blocage ici
-              await sendMail(transporter, relanceMail);
-              console.log(
-                `L'email de relance pour l'entreprise ${offer.company} a bien été envoyé !`
-              );
-            }
-          }
-        } catch (error) {
-          console.error("Erreur lors de la vérification des offres : ", error);
-        }
-      };
-
-      // await sendScheduledEmails();
-
-      const fetchData = async () => {
-        try {
-          // Attendre que la requête s'exécute et récupère les données
-          const offers = await offerModel.find().sort({ createdAt: -1 });
-          console.log("Toutes les offres : ", offers);
-          console.log("Nombre d'offres : ", offers.length);
-        } catch (error) {
-          console.error("Erreur lors de la récupération des offres : ", error);
-        }
-      };
-
-      fetchData();
-
-      // Envoyer la réponse une fois la tâche exécutée
-      res.status(200).json({ message: "Scheduled task executed successfully" });
+      // Envoyer la réponse une fois que les emails ont été envoyés
+      res
+        .status(200)
+        .json({ message: "Emails de relance envoyés avec succès." });
     } catch (error) {
       console.error(
-        "Erreur de connexion SMTP ou d'exécution de la tâche planifiée",
+        "Erreur lors de l'exécution de la tâche planifiée :",
         error
       );
       res.status(500).json({
-        message: "Error executing scheduled task",
+        message: "Erreur lors de l'exécution de la tâche planifiée.",
         error: error.message,
       });
     }
