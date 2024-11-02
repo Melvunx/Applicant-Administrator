80f091dc541b19db241354b1cce6b1f8d0f1c3dc
Melvunx <augustine.melv@gmmail.com>
2024-11-02 10:21:15 +0100

    cron cange 2

--- server/api/cron.js
+++ server/api/cron.js
@@ -1,11 +1,5 @@
 import { sendScheduledEmails } from "..";
 
-export default async function handler(req, res) {
-  if (
-    req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
-  ) {
-    return res.status(401).end("Unauthorized");
-  } else {
-    await sendScheduledEmails();
-  }
+export default async function handler() {
+  await sendScheduledEmails();
 }
