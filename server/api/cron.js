651221986f0e3afd5f24b95c65184ae84d94e97f
Melvunx <augustine.melv@gmmail.com>
2024-11-02 12:22:50 +0100

    test fix number 2

--- server/api/cron.js
+++ server/api/cron.js
@@ -43,83 +43,78 @@ export default async function handler(req, res) {
 
         console.log("Date de comparaison : ", oneWeekAgo);
 
-        const MAX_RETRIES = 3;
+        const timeoutPromise = new Promise((_, reject) => {
+          setTimeout(
+            () => reject(new Error("La requête a dépassé le temps imparti")),
+            5000
+          ); // 5 secondes
+        });
 
-        const fetchOffers = async (retryCount = 0) => {
-          try {
-            return await offerModel.find({
-              applyDate: { $lte: oneWeekAgo },
-              archived: false,
-              status: "Envoyé",
-            });
-          } catch (error) {
-            if (retryCount < MAX_RETRIES) {
-              console.log(
-                `Tentative de nouveau... Essai numéro ${retryCount + 1}`
-              );
-              return fetchOffers(retryCount + 1);
-            } else {
-              console.error(
-                "Erreur lors de la récupération des offres : ",
-                error
-              );
-              throw error; // Relancer l'erreur pour que l'appelant puisse gérer
-            }
-          }
-        };
+        try {
+          const offers = await Promise.race([
+            offerModel
+              .find({
+                applyDate: { $lte: oneWeekAgo },
+                archived: false,
+                status: "Envoyé",
+              })
+              .exec(),
+            timeoutPromise,
+          ]);
 
-        if (fetchOffers.length === 0) {
-          console.log("Aucune offre trouvée pour l'envoi de mail");
-        } else {
-          for (const offer of fetchOffers) {
-            console.log("Traitement de l'offre : ", offer.company);
+          console.log("Offres récupérées : ", offers);
 
-            const relanceMail = {
-              from: {
-                name: "Rappeleur de relance",
-                address: process.env.USER_ADDRESS,
-              },
-              to: process.env.USER_ADDRESS,
-              subject: `Relance de Candidature - Une semaine depuis ma candidature chez ${offer.company}`,
-              html: `
-              <div style="font-family: Arial, sans-serif; color: #333; padding: 20px; background-color: #f9f9f9; border: 1px solid #ddd; border-radius: 10px; max-width: 600px; margin: auto;">
-              <h1 style="font-size: 24px; color: #4CAF50;">Bonjour mon Melv,</h1>
-              <p style="font-size: 16px; line-height: 1.6;">
-              Cela fait <strong>une semaine</strong> que tu as postulé chez 
-              <strong>${offer.company}</strong> avec 
-              ${
-                offer.type === "Candidature spontanée"
-                  ? "<em>une candidature spontanée</em>"
-                  : `l'offre <strong>${offer.title}</strong>`
-              }.
-              </p>
-              <p style="font-size: 16px; line-height: 1.6;">
-              Il est temps de relancer ta candidature en renvoyant un e-mail. Ne perds pas de temps !
-              </p>
-              <div style="margin-top: 20px; text-align: center;">
-              <a href="${
-                offer.url
-              }" style="display: inline-block; padding: 12px 24px; background-color: #4CAF50; color: white; text-decoration: none; border-radius: 5px; font-size: 16px;">Voir l'offre</a>
-              </div>
-              <br/>
-              <p style="font-size: 16px; line-height: 1.6;">
-              Cordialement,<br/>
-              Toi même.<br/>
-              </p>
-              </div>
-              `,
-            };
+          if (offers.length === 0) {
+            console.log("Aucune offre trouvée pour l'envoi de mail");
+          } else {
+            for (const offer of offers) {
+              console.log("Traitement de l'offre : ", offer.company);
 
-            // Enlever `await` pour tester s'il s'agit d'un blocage ici
-            try {
+              const relanceMail = {
+                from: {
+                  name: "Rappeleur de relance",
+                  address: process.env.USER_ADDRESS,
+                },
+                to: process.env.USER_ADDRESS,
+                subject: `Relance de Candidature - Une semaine depuis ma candidature chez ${offer.company}`,
+                html: `
+                  <div style="font-family: Arial, sans-serif; color: #333; padding: 20px; background-color: #f9f9f9; border: 1px solid #ddd; border-radius: 10px; max-width: 600px; margin: auto;">
+                    <h1 style="font-size: 24px; color: #4CAF50;">Bonjour mon Melv,</h1>
+                    <p style="font-size: 16px; line-height: 1.6;">
+                      Cela fait <strong>une semaine</strong> que tu as postulé chez 
+                      <strong>${offer.company}</strong> avec 
+                      ${
+                        offer.type === "Candidature spontanée"
+                          ? "<em>une candidature spontanée</em>"
+                          : `l'offre <strong>${offer.title}</strong>`
+                      }.
+                    </p>
+                    <p style="font-size: 16px; line-height: 1.6;">
+                      Il est temps de relancer ta candidature en renvoyant un e-mail. Ne perds pas de temps !
+                    </p>
+                    <div style="margin-top: 20px; text-align: center;">
+                      <a href="${
+                        offer.url
+                      }" style="display: inline-block; padding: 12px 24px; background-color: #4CAF50; color: white; text-decoration: none; border-radius: 5px; font-size: 16px;">Voir l'offre</a>
+                    </div>
+                    <br/>
+                    <p style="font-size: 16px; line-height: 1.6;">
+                      Cordialement,<br/>
+                      Toi même.<br/>
+                    </p>
+                  </div>
+                `,
+              };
+
+              // Enlever `await` pour tester s'il s'agit d'un blocage ici
               await sendMail(transporter, relanceMail);
               console.log(
                 `L'email de relance pour l'entreprise ${offer.company} a bien été envoyé !`
               );
-            } catch (error) {
-              console.error("Erreur lors de l'envoie des offres : ", error);
             }
           }
+        } catch (error) {
+          console.error("Erreur lors de la vérification des offres : ", error);
         }
       };
 
