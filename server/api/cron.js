714ea0035970eabecd9730a2d2bf6c47b4a89b47
Melvunx <augustine.melv@gmmail.com>
2024-11-02 10:28:06 +0100

    change path vercel

--- server/api/cron.js
+++ server/api/cron.js
@@ -1,5 +1,14 @@
-import { sendScheduledEmails } from "..";
+import { sendScheduledEmails } from "../index";
 
-export default async function handler() {
-  await sendScheduledEmails();
+export default async function handler(req, res) {
+  if (
+    req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return res.status(401).end("Unauthorized");
+  } else if (req.method === "GET") {
+    await sendScheduledEmails();
+    res.status(200).json({ message: "Scheduled task executed successfully" });
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
 }
