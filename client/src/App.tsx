273b934bc82f4b7d3bd7c0298f875890e39c8502
Melvunx <augustine.melv@gmmail.com>
2024-11-23 18:11:09 +0100

    setup login/register page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Archive from "./Pages/Archive";
+import Auth from "./Pages/Auth";
 import Home from "./Pages/Home";
 
 function App() {
@@ -7,8 +8,9 @@ function App() {
     <div>
       <Routes>
         <Route path="/Applicant-Administrator/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
         <Route path="/Applicant-Administrator/archive" element={<Archive />} />
-        <Route path="*" element={<Home />} />
+        <Route path="*" element={<Auth />} />
       </Routes>
     </div>
   );
