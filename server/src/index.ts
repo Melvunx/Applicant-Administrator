7105ce5d369758398c57aaa75b98b5994c8f48a9
Melvunx <augustine.melv@gmmail.com>
2025-02-01 12:05:16 +0100

    setup files

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,34 @@
 require("dotenv").config();
+import cookieParser from "cookie-parser";
 import express from "express";
+import colors from "./schema/colors.schema";
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const { PORT } = process.env;
 
+if (!PORT) {
+  throw new Error("Port number not found");
+}
+
 const app = express();
 
+app.use(cookieParser());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${Number(PORT)}`);
+  console.log(
+    colors.info(`\nServer running on port http://localhost:${Number(PORT)}`)
+  );
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
---
+++ server/src/services/api.services.ts
@@ -0,0 +1,65 @@
+import {
+  handleResponseError,
+  handleResponseSuccess,
+} from "@/utils/api.response";
+import { Response } from "express";
+
+class ApiResponse {
+  constructor() {}
+
+  success(
+    response: Response,
+    status: "Ok" | "Created",
+    data?: any,
+    message = "Request succeded"
+  ) {
+    let code: number;
+    switch (status) {
+      case "Created":
+        code = 201;
+        break;
+      default:
+        code = 200;
+        break;
+    }
+
+    response.status(code).json(handleResponseSuccess(data, message));
+  }
+
+  error(
+    response: Response,
+    status:
+      | "Internal Server Error"
+      | "Unauthorized"
+      | "Not Found"
+      | "Bad Request"
+      | "Forbidden",
+    error?: any,
+    message = "Request failed"
+  ) {
+    let code: number;
+    switch (status) {
+      case "Bad Request":
+        code = 400;
+        break;
+      case "Not Found":
+        code = 404;
+        break;
+      case "Unauthorized":
+        code = 401;
+        break;
+      case "Forbidden":
+        code = 403;
+        break;
+      default:
+        code = 500;
+        break;
+    }
+
+    response.status(code).json(handleResponseError(error, message));
+  }
+}
+
+const apiReponse = new ApiResponse();
+
+export default apiReponse;
